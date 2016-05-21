3d624307dc13f90968f26a64d46d2570d9387445
Mirai Hattori(2015) <mirai2045@gmail.com>
2016-05-21 10:20:07 +0900

    cppを完成

--- exec.sh
+++ exec.sh
@@ -2,7 +2,9 @@
 g++ ai.cpp -o AI -std=c++11 -Wall -Wextra -Wconversion
 if [ ! -e logs -a -f 2048/2048.rb ]; then
     mkdir logs
-elif [ -e logs ]; then
+fi
+
+if [ -e logs ]; then
     ruby 2048/2048.rb -exec "./AI"
 else
     echo "aiプログラムやlogs等があるディレクトリで実行してください"
