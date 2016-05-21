56a0837e388bf156b1e11bc1c54a60eb0ea91a2f
Mirai Hattori(2015) <mirai2045@gmail.com>
2016-05-21 10:21:17 +0900

    Cを完成

--- exec.sh
+++ exec.sh
@@ -2,7 +2,8 @@
 gcc ai.c -o AI -std=c99 -Wall -Wextra -Wconversion -D _BSD_SOURCE
 if [ ! -e logs -a -f 2048/2048.rb ]; then
     mkdir logs
-elif [ -e logs ]; then
+fi
+if [ -e logs ]; then
     ruby 2048/2048.rb -exec "./AI"
 else
     echo "aiプログラムやlogs等があるディレクトリで実行してください"
