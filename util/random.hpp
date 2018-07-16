1b2d7d8ab3af7da87287badb73dab13f012563af
Mirai Hattori <mirai2045@gmail.com>
2018-07-16 20:59:53 +0900

    add random test

--- util/random.hpp
+++ util/random.hpp
@@ -10,7 +10,7 @@ T randUniform(T min, T max)
 {
     static std::random_device seed;
     static std::mt19937 rand_generator(seed());
-    std::uniform_real_distribution<double> rand(min, max);
+    std::uniform_real_distribution<double> rand(min, max - std::numeric_limits<double>::epsilon());
     if constexpr (std::numeric_limits<T>::is_integer) {
         return static_cast<int>(rand(rand_generator));
     } else {
