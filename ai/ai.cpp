1d8572cf11bdbd7e6285a0607d6a2c0d585d7871
Mirai Hattori <mirai2045@gmail.com>
2018-07-16 21:01:03 +0900

    modify ai/ai.cpp

--- ai/ai.cpp
+++ ai/ai.cpp
@@ -2,11 +2,13 @@
 
 #include <iomanip>
 #include <iostream>
+#include <limits>
 
 #include <Eigen/Core>
 
 #include "ai/full_connected_network.hpp"
 #include "board/board.hpp"
+#include "util/random.hpp"
 
 namespace AI
 {
@@ -22,13 +24,14 @@ namespace AI
 */
 
 std::shared_ptr<GD> gd = nullptr;
-std::unique_ptr<FullConnectedNetwork<SoftmaxWithLoss<4, 4>, Affine<4, 10>, Relu<10, 10>, Affine<10, 4>>> q = nullptr;
+using QNet = FullConnectedNetwork<SoftmaxWithLoss<4, 4>, Affine<4, 10>, Relu<10, 10>, Affine<10, 4>>;
+std::unique_ptr<QNet> q = nullptr;
 
 void init()
 {
     // initialization
     gd = std::make_shared<GD>(0.01);
-    q = std::make_unique<FullConnectedNetwork<SoftmaxWithLoss<4, 4>, Affine<4, 10>, Relu<10, 10>, Affine<10, 4>>>(gd);
+    q = std::make_unique<QNet>(gd);
 }
 
 void initGame()
@@ -39,6 +42,7 @@ void initGame()
 void chooseMove()
 {
     using Board::board;
+    using Board::Manipulation;
     using Params::ROW_SIZE;
     using Params::COL_SIZE;
 
@@ -75,14 +79,41 @@ void chooseMove()
     */
 
     /*
-     * target = r(s, a) + 0.99 * q(s', a');
-     * loss = target - q(s, a);
-     * q(s, a) = q(s, a) + 0.1 * loss;
+     * q(s, a)にsを入れて
      * percentage = randUniform(0.0, 100.0);
      * if (percentage < EPSILON) {
      *     a = random;
      * }
+     * target = r(s, a) + 0.99 * q(s', a');
+     * loss = target - q(s, a);
+     * q(s, a) = q(s, a) + 0.1 * loss;
      */
+
+    // NNに入れる状態s(ROW_SIZE * COL_SIZE次元のEigen::VectorXd)に変換
+    Eigen::VectorXd s = Eigen::VectorXd::Zero(ROW_SIZE * COL_SIZE);
+    const auto& tmp = board->boardArray();
+    for (std::size_t i = 0; i < ROW_SIZE; i++) {
+        for (std::size_t j = 0; i < COL_SIZE; j++) {
+            s[i * COL_SIZE + j] = tmp[i][j];
+        }
+    }
+    /*
+     * 0: Up, 1: Left, 2: Down, 3:Right
+     */
+    Eigen::VectorXd/* 4d */ acts = q->predict(s);
+    double max = std::numeric_limits<double>::min();
+    Manipulation manip = Manipulation::None;
+    for (int i = 0; i < acts.size(); i++) {
+        if (max < acts[i]) {
+            max = acts[i];
+            manip = static_cast<Manipulation>(i);
+        }
+    }
+    double percentage = Util::randUniform(0.0, 100.0);
+    if (percentage < 10) {
+        manip = static_cast<Manipulation>(Util::randUniform(0, 4));
+    }
+
 }
 
 
